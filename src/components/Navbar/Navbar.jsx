e5464bcbcdb6e0fcd74e14369390ead9babb7ccc
John Mwaura <johnmwaura08@gmail.com>
2020-10-21 12:44:06 -0500

    indentation using tabs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,83 +1,63 @@
 import React from "react";
-import {TweenMax} from "gsap";
-import styles from "./Navbar.module.css"
+import { TweenMax } from "gsap";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
- 
   function enterHomeButton() {
-  
-      TweenMax.to(`.fa-home`, 0.4, { color: "#181818" });
-      TweenMax.to(`.p-home`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-  
+    TweenMax.to(`.fa-home`, 0.4, { color: "#181818" });
+    TweenMax.to(`.p-home`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
   }
 
   function leaveHomeButton() {
-   
-      TweenMax.to(`.fa-home`, 0.4, {
-        color: "#42474b",
-      });
-      TweenMax.to(`.p-home`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-  
+    TweenMax.to(`.fa-home`, 0.4, {
+      color: "#42474b",
+    });
+    TweenMax.to(`.p-home`, 0.4, { color: "red", opacity: 0 }, "-=.4");
   }
   function enterAboutButton() {
-   
     TweenMax.to(`.fa-user`, 0.4, { color: "#181818" });
     TweenMax.to(`.p-user`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-    
   }
-   function leaveAboutButton() {
-    
-     TweenMax.to(`.fa-user`, 0.4, {
-       color: "#42474b",
-     });
-     TweenMax.to(`.p-user`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-    
-   }
-    function enterSkillsButton() {
-      TweenMax.to(`.fa-cog`, 0.4, { color: "#181818" });
-      TweenMax.to(`.p-cog`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-    }
-    function leaveSkillsButton() {
-      TweenMax.to(`.fa-cog`, 0.4, {
-        color: "#42474b",
-      });
-      TweenMax.to(`.p-cog`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-	}
+  function leaveAboutButton() {
+    TweenMax.to(`.fa-user`, 0.4, {
+      color: "#42474b",
+    });
+    TweenMax.to(`.p-user`, 0.4, { color: "red", opacity: 0 }, "-=.4");
+  }
+  function enterSkillsButton() {
+    TweenMax.to(`.fa-cog`, 0.4, { color: "#181818" });
+    TweenMax.to(`.p-cog`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+  }
+  function leaveSkillsButton() {
+    TweenMax.to(`.fa-cog`, 0.4, {
+      color: "#42474b",
+    });
+    TweenMax.to(`.p-cog`, 0.4, { color: "red", opacity: 0 }, "-=.4");
+  }
 
-	 function enterProjectsButton() {
-     TweenMax.to(`.fa-eye`, 0.4, { color: "#181818" });
-     TweenMax.to(`.p-eye`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-   }
-   function leaveProjectsButton() {
-     TweenMax.to(`.fa-eye`, 0.4, {
-       color: "#42474b",
-     });
-     TweenMax.to(`.p-eye`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-   }
-   	 function enterContactButton() {
-       TweenMax.to(`.fa-eye`, 0.4, { color: "#181818" });
-       TweenMax.to(`.p-eye`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-     }
-     function leaveContactButton() {
-       TweenMax.to(`.fa-envelope`, 0.4, {
-         color: "#42474b",
-       });
-       TweenMax.to(
-         `.p-envelope`,
-         0.4,
-         { color: "#00ffff", opacity: 0 },
-         "-=.4"
-       );
-     }
-
-
-
- 
+  function enterProjectsButton() {
+    TweenMax.to(`.fa-eye`, 0.4, { color: "#181818" });
+    TweenMax.to(`.p-eye`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+  }
+  function leaveProjectsButton() {
+    TweenMax.to(`.fa-eye`, 0.4, {
+      color: "#42474b",
+    });
+    TweenMax.to(`.p-eye`, 0.4, { color: "red", opacity: 0 }, "-=.4");
+  }
+  function enterContactButton() {
+    TweenMax.to(`.fa-eye`, 0.4, { color: "#181818" });
+    TweenMax.to(`.p-eye`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+  }
+  function leaveContactButton() {
+    TweenMax.to(`.fa-envelope`, 0.4, {
+      color: "#42474b",
+    });
+    TweenMax.to(`.p-envelope`, 0.4, { color: "#00ffff", opacity: 0 }, "-=.4");
+  }
 
   return (
-    <div
-      className={styles.container}
-    >
+    <div className={styles.container}>
       <div className={styles.navContainer}>
         <i
           className={`fas fa-lg fa-home`}
