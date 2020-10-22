fbd28cda52b363771950cac2cafc8d8d94d735cc
John Mwaura <johnmwaura08@gmail.com>
2020-10-22 00:48:37 -0500

    projects component and css module

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Projects.module.css'
+
+function Projects() {
+	return (
+		<div className={styles.projectsContainer}>
+			<h1>
+
+			This is the projects page
+			</h1>
+		</div>
+	)
+}
+
+export default Projects;
