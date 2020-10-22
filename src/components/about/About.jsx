0b2d017070a0e5ab91d97f8c9770f2a68c7b2bd6
John Mwaura <johnmwaura08@gmail.com>
2020-10-22 00:46:41 -0500

    about component and css module

---
+++ src/components/about/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './About.module.css';
+
+function About() {
+	return (
+		<div className={styles.aboutContainer}>
+			<h1>
+
+			This is the about page
+			</h1>
+		</div>
+	)
+}
+
+export default About;
