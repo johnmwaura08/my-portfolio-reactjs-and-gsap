24e1bbc810b3d923557d2ac9386347b9a0d2a55b
John Mwaura <johnmwaura08@gmail.com>
2020-10-22 00:47:02 -0500

    contact component and css module

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Contact.module.css'
+
+function Contact() {
+	return (
+		<div className={styles.contactContainer}>
+			<h1>
+
+			This is the contact page
+			</h1>
+		</div>
+	)
+}
+
+export default Contact;
