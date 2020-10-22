a3907b54114dd7f15f6fd25b76c00d9d3815fc3c
John Mwaura <johnmwaura08@gmail.com>
2020-10-22 00:42:37 -0500

    imported all the components and used react-router for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
-import Carousel from '../src/components/Carousel';
-import Navbar from './components/Navbar/Navbar'
-
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "../src/components/home/Home";
+import About from "../src/components/about/About";
+import Skills from "../src/components/skills/Skills";
+import Projects from "../src/components/projects/Projects";
+// import Carousel from "../src/components/skills/Carousel";
+import Contact from "../src/components/contact/Contact";
+import "./App.css";
 
 function App() {
   return (
-	<Navbar />
+    <main className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          {/* <Route path="/images" component={Carousel} /> */}
+        </Switch>
+      </Router>
+    </main>
   );
 }
 
