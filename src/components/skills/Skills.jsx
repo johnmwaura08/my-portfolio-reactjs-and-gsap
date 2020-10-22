ab3491d6c328a32aa901060b0ac4fa595d83f3bd
John Mwaura <johnmwaura08@gmail.com>
2020-10-22 00:49:04 -0500

    skills component and css module

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Skills.module.css';
+
+function Skills() {
+	return (
+		<div className={styles.skillsContainer}>
+			<h1>
+
+			This is the skills page
+			</h1>
+		</div>
+	)
+}
+
+export default Skills
