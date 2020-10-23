a41861fd5ea50831dfec7720d303c599ae381b8a
John Mwaura <johnmwaura08@gmail.com>
2020-10-22 22:15:58 -0500

    added projects with material ui and custom css

---
+++ src/components/projects/ProjectSection.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import CodeIcon from "@material-ui/icons/Code";
+import projects from "./ProjectsList";
+
+const projectsArray = projects;
+
+console.log(projectsArray);
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    justifyContent: "space-around",
+    display: "flex",
+    flexWrap: "wrap",
+    backGroundColor: "#37393b",
+  },
+  root: {
+    maxWidth: 700,
+    display: "grid",
+    margin: "70px",
+    backGroundColor: "#37393b",
+    backGround: "#37393b",
+    borderRadius: "10px",
+    boxShadow: "inset 0 0 10px rgba(0, 0, 0, 0.5)",
+    flexDirection: "column",
+    fontFamily: "Poppins, sans-serif !important",
+    color: "#37393b",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: "#00ffff",
+    color: "black",
+  },
+  font: {
+    fontFamily: "Poppins, sans-serif !important",
+    fontSize: "17px",
+    fontWeight: "800",
+  },
+
+  btnContainer: {
+    // backgroundColor: red[500],
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "100%",
+  },
+  legacybtn: {
+    // backgroundColor: red[500],
+    display: "block",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    fontFamily: "Poppins, sans-serif !important",
+    fontSize: "17px",
+    padding: "8px",
+    color: "#37393b",
+    border: "2px solid #00ffff !important",
+
+    borderRadius: "5px",
+
+    fontWeight: "500",
+    "&:hover": {
+      backgroundColor: "#00ffff",
+      color: "black",
+    },
+  },
+  iconbtn: {
+    fontFamily: "Poppins, sans-serif !important",
+    fontSize: "17px",
+    padding: "8px",
+    color: "#37393b",
+    border: "2px solid #00ffff !important",
+    width: "100px",
+    borderRadius: "5px",
+    justifyContent: "space-evenly",
+    fontWeight: "500",
+    "&:hover": {
+      backgroundColor: "#00ffff",
+      color: "black",
+    },
+  },
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+
+  return (
+    <>
+      {projectsArray.map((project, index) => (
+        <div className={classes.container} key={index}>
+          <Card className={classes.root}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="recipe" className={classes.avatar}>
+                  {project.name}
+                </Avatar>
+              }
+              title={project.name}
+             
+            />
+            <CardMedia
+              className={classes.media}
+              image={project.image}
+              title={project.name}
+            />
+            <CardContent>
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                component="p"
+                className={classes.font}
+              >
+                {project.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <div className={classes.btnContainer}>
+                {project.name === "Legacy Financial Dashboard" ? (
+                  <IconButton className={classes.legacybtn}>
+                    <CodeIcon /> Source code availabe on request
+                  </IconButton>
+                ) : (
+                  <>
+                    <IconButton className={classes.iconbtn}>
+                      <VisibilityIcon /> Demo
+                    </IconButton>
+
+                    <IconButton className={classes.iconbtn}>
+                      <CodeIcon /> Source
+                    </IconButton>
+                  </>
+                )}
+              </div>
+            </CardActions>
+          </Card>
+        </div>
+      ))}
+    </>
+  );
+}
+
---
+++ src/components/projects/ProjectsList.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import covidgif from "../../assets/images/covidgif.gif";
+import githubgif from "../../assets/images/githubgif.gif";
+import shippinggif from "../../assets/images/shippinggif.gif";
+import portfoliogif from "../../assets/images/portfoliogif.gif";
+import legacygif from "../../assets/images/legacygif.gif";
+
+const projects = [
+  {
+    name: "COVID-19 TRACKER",
+    description: (
+      <p>
+        This app displays the most current data of Covid-19 cases worldwide.
+        Data includes number of deaths, number of infections and number of
+        recoveries. You can also select a country and see it's corresponding
+        data. I've used ChartsJs to display the data and ReactJS to fetch the
+        data from an API.
+      </p>
+    ),
+    image: covidgif,
+  },
+  {
+    name: "Wavy Kits",
+    description: (
+      <p>
+        This is an e-commerce store for original kits from some of the biggest
+        soccer clubs in the world.The landing page was designed using figma and
+        implemented in React. The cards were designed using styled components.
+        The user can click on a kit, see details and add to cart. I've
+        implemented react-paypal-express for check-out using paypal.
+      </p>
+    ),
+    image: shippinggif,
+  },
+  {
+    name: "Github Members",
+    description: (
+      <p>
+        This app utilises the Github Api. The user logs in and can search for
+        github users and see data such as number of repositories, number of
+        followers, most forked repositories, largest repositories, and most used
+        languages. Since the number of searches per hour is limited by IP
+        address, the user can also see the number of api requests they have
+        left. I've used authO for user sign up and sign in using social
+        connections such as google, twitter or github. The user can also create
+        an account and sign in . I've used Fusion Charts to display the data
+        from Github APi. This app is built using ReactJS
+      </p>
+    ),
+    image: githubgif,
+  },
+  {
+    name: "Legacy Financial Dashboard",
+    description: (
+      <p>
+        I was part of a team hired to build a dashboard for a local company. We
+        used the MERN stack during production. The project was styled using
+        material-UI on the front-end. User authentication on the backend was
+        done using PassportJS. The project was deployed using Google Cloud
+        Platform
+      </p>
+    ),
+    image: legacygif,
+  },
+  {
+    name: "Portfolio Website",
+    description: (
+      <p>
+        This is my portfolio website i designed to display my projects and
+        skills.The portfolio is built with ReactJS and includes hooks. I
+        included gifs from Lottie Files and animated the project using gsap. I
+        used Greensock's Timeline max to build a rotating cube that dynamically
+        displays the languages, frameworks and tools i'm proficient in. I used
+        custom css to design a modern looking site that's aesthetically pleasing
+        and minimalistic. I used Material-UI to design a simple responsive
+        contact form and FormSpree as the form backend and email service.
+      </p>
+    ),
+    image: portfoliogif,
+  },
+];
+
+export default projects;
