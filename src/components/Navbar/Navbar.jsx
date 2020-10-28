2edc23f4ae8742715aa3226282219587bdfe41f7
John Mwaura <johnmwaura08@gmail.com>
2020-10-28 12:55:35 -0500

    spacing on smaller screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,74 +3,95 @@ import { TweenMax } from "gsap";
 import { useHistory } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
- function Navbar() {
-
+function Navbar() {
   const history = useHistory();
-	
-  const enterHomeButton = () => {
-    TweenMax.to(`.fa-home`, 0.4, { color: "#181818" });
-    TweenMax.to(`.p-home`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+
+  if (window.innerWidth > 1000) {
   }
+
+  const enterHomeButton = () => {
+    if (window.innerWidth > 1000) {
+      TweenMax.to(`.fa-home`, 0.4, { color: "#181818" });
+      TweenMax.to(`.p-home`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+    } else {
+      TweenMax.to(`.fa-home`, 0.4, { color: "#00ffff" });
+    }
+  };
 
   const leaveHomeButton = () => {
     TweenMax.to(`.fa-home`, 0.4, {
       color: "#42474b",
     });
     TweenMax.to(`.p-home`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-  } 
+  };
 
   const enterAboutButton = () => {
-    TweenMax.to(`.fa-user`, 0.4, { color: "#181818" });
-    TweenMax.to(`.p-user`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-  }
+    if (window.innerWidth > 1000) {
+      TweenMax.to(`.fa-user`, 0.4, { color: "#181818" });
+      TweenMax.to(`.p-user`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+    } else {
+      TweenMax.to(`.fa-user`, 0.4, { color: "#00ffff" });
+    }
+  };
 
   const leaveAboutButton = () => {
     TweenMax.to(`.fa-user`, 0.4, {
       color: "#42474b",
     });
     TweenMax.to(`.p-user`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-  }
+  };
 
   const enterSkillsButton = () => {
-    TweenMax.to(`.fa-cog`, 0.4, { color: "#181818" });
-    TweenMax.to(`.p-cog`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-  }
+    if (window.innerWidth > 1000) {
+      TweenMax.to(`.fa-cog`, 0.4, { color: "#181818" });
+      TweenMax.to(`.p-cog`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+    } else {
+      TweenMax.to(`.fa-cog`, 0.4, { color: "#00ffff" });
+    }
+  };
 
   const leaveSkillsButton = () => {
     TweenMax.to(`.fa-cog`, 0.4, {
       color: "#42474b",
     });
     TweenMax.to(`.p-cog`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-  }
+  };
 
   const enterProjectsButton = () => {
-    TweenMax.to(`.fa-eye`, 0.4, { color: "#181818" });
-    TweenMax.to(`.p-eye`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-  }
+    if (window.innerWidth > 1000) {
+      TweenMax.to(`.fa-eye`, 0.4, { color: "#181818" });
+      TweenMax.to(`.p-eye`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+    } else {
+      TweenMax.to(`.fa-eye`, 0.4, { color: "#00ffff" });
+    }
+  };
 
   const leaveProjectsButton = () => {
     TweenMax.to(`.fa-eye`, 0.4, {
       color: "#42474b",
     });
     TweenMax.to(`.p-eye`, 0.4, { color: "red", opacity: 0 }, "-=.4");
-  }
+  };
 
   const enterContactButton = () => {
-    TweenMax.to(`.fa-eye`, 0.4, { color: "#181818" });
-    TweenMax.to(`.p-eye`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
-  }
+    if (window.innerWidth > 1000) {
+      TweenMax.to(`.fa-envelope`, 0.4, { color: "#181818" });
+      TweenMax.to(`.p-envelope`, 0.4, { color: "#00ffff", opacity: 1 }, "-=.4");
+    } else {
+      TweenMax.to(`.fa-envelope`, 0.4, { color: "#00ffff" });
+    }
+  };
 
   const leaveContactButton = () => {
     TweenMax.to(`.fa-envelope`, 0.4, {
       color: "#42474b",
     });
-    TweenMax.to(`.p-envelope`, 0.1, { color: "#00ffff", opacity: 0 },);
-  }
+    TweenMax.to(`.p-envelope`, 0.1, { color: "#00ffff", opacity: 0 });
+  };
 
   const handleMenuItemClick = (pageURL) => {
     history.push(pageURL);
   };
-
 
   return (
     <div className={styles.container}>
